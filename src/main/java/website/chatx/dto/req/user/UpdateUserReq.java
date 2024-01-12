f15ef72ffc4b58f306ad9361dfd87bf1e9db1a05
cuongnh2k <cuongnh2k@gmail.com>
2024-01-12 16:50:20 +0700

    update

--- src/main/java/website/chatx/dto/req/user/UpdateUserReq.java
+++ src/main/java/website/chatx/dto/req/user/UpdateUserReq.java
@@ -1,6 +1,5 @@
 package website.chatx.dto.req.user;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +7,6 @@ import lombok.Setter;
 @Setter
 public class UpdateUserReq {
 
-    @NotBlank
     private String name;
 
     private String avatarUrl;
